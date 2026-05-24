4bf2f7b954d0ea60f37f40ce475059c187d85ffd
Gautam Singh <gautamsingh12122003@gmail.com>
2026-05-24 12:38:13 +0530

    learned about SQLite DB and it's APIs in expo

--- 05-data-storage-and-offline/src/app/index.tsx
+++ 05-data-storage-and-offline/src/app/index.tsx
@@ -1,10 +1,35 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import * as SecureStore from "expo-secure-store";
+import * as SQLite from "expo-sqlite";
 import { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
+// Creates db if it doesn't exist
+const db = SQLite.openDatabaseSync("demo.db");
+
 export default function Index() {
+    const [output, setOutput] = useState("");
     const [data, setData] = useState("");
+
+    const createTable = () => {
+        db.execSync(`
+            CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            age INTEGER    
+            )
+        `);
+    };
+
+    const getUsers = () => {
+        const users = db.getAllSync("SELECT * FROM users");
+
+        setOutput(JSON.stringify(users, null, 2));
+    };
+
+    const insertData = () => {
+        db.runSync("INSERT INTO users (name, age) VALUES (?, ?)", "Gautam", 22);
+    };
 
     // setItem
     const saveData = async () => {
